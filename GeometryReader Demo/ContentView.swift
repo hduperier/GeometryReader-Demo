ef8b299d25f914f5d4bb07dd80589f9c489076c2
hduperier <59696097+hduperier@users.noreply.github.com>
2021-07-20 19:28:43 -0400

    playing with offset and padding

--- GeometryReader Demo/ContentView.swift
+++ GeometryReader Demo/ContentView.swift
@@ -15,23 +15,19 @@ struct ContentView: View {
             VStack {
                 GeometryReader {geo in
                     Rectangle()
-                        .frame(width: geo.size.width/4, height: geo.size.height/8, alignment: .center)
+                        .frame(width: geo.size.width/4, height: geo.size.height, alignment: .center)
                         .onTapGesture {
                             print("x: \(geo.frame(in: .global).minX) , y: \(geo.frame(in: .global).minY)")
                             print("x: \(geo.frame(in: .local).minX) , y: \(geo.frame(in: .local).minY)")
                         }
                     
-                    Rectangle()
-                        .foregroundColor(.red)
-                        .frame(width: 100, height: 100, alignment: .center)
-                        //.padding([.leading, .top], 30)
-                        .offset(x:30, y:30)
-                }
+                    
+                }.padding([.top], 30)
                 
                 GeometryReader {geo in
                 Rectangle()
                     .foregroundColor(.green)
-                    .frame(width: geo.size.width/4, height: geo.size.height/8, alignment: .center)
+                    .frame(width: geo.size.width/4, height: geo.size.height, alignment: .center)
                     .onTapGesture {
                         print("x: \(geo.frame(in: .global).minX) , y: \(geo.frame(in: .global).minY)")
                         print("x: \(geo.frame(in: .local).minX) , y: \(geo.frame(in: .local).minY)")
